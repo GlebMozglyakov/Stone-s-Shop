5016e872edc3bda253ed24cb2923ce51f6316551
GlebMozglyakov <mozglgleb2004@gmail.com>
2023-06-06 20:21:07 +0500

    Change architecture

---
+++ Rocky_Utiliti/WC.cs
@@ -0,0 +1,15 @@
+﻿namespace Rocky_Utility
+{
+    public static class WC
+    {
+        public const string ImagePath = @"\images\product\";
+
+        public const string SessionCart = "ShoppingCartSession";
+
+        public const string AdminRole = "Admin";
+
+        public const string CustomerRole = "Customer";
+
+        public const string EmailAdmin = "mozglgleb2004@gmail.com";
+    }
+}
